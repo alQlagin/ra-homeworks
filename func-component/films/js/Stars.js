2f083a2e1cc0a279198d2d3534628e6a7aa213a4
Alex Kulagin <al.qlagin@gmail.com>
2018-03-20 15:29:12 +0500

    added Stars component

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,13 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+    if (count < 1 || count > 5) return null;
+    const stars = [];
+    do {
+        stars.push(count);
+        count--;
+    } while (count > 0);
+    return <ul className="card-body-stars u-clearfix">
+        <li>{stars.map(() => <Star/>)}</li>
+    </ul>;
 }
