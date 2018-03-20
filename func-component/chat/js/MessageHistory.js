804d042fe68b593999aa36f9f48e627a212f293a
Alex Kulagin <al.qlagin@gmail.com>
2018-03-20 15:35:37 +0500

    added MessageHistory component

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,10 @@
 'use strict';
+function MessageHistory({list}) {
+    return <ul>{list.map(message => {
+        switch (message.type) {
+            case 'message': return <Message from={message.from} message={message}></Message>;
+            case 'response': return <Response from={message.from} message={message}></Response>;
+            case 'typing': return <Typing from={message.from} message={message}></Typing>;
+        }
+    })}</ul>
+}
