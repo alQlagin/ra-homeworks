445835598fb253d5992c7571745fcbb50ba0b62b
Alex Kulagin <al.qlagin@gmail.com>
2018-03-21 12:18:05 +0500

    update MessageHistory one return value with key attribute

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,10 +1,22 @@
 'use strict';
+
 function MessageHistory({list}) {
     return <ul>{list.map(message => {
+        let MessageContainer;
         switch (message.type) {
-            case 'message': return <Message from={message.from} message={message}></Message>;
-            case 'response': return <Response from={message.from} message={message}></Response>;
-            case 'typing': return <Typing from={message.from} message={message}></Typing>;
+            case 'message':
+                MessageContainer = Message;
+                break;
+            case 'response':
+                MessageContainer = Response;
+                break;
+            case 'typing':
+                MessageContainer = Typing;
+                break;
+            default:
+                return null;
         }
+
+        return <MessageContainer key={message.id} from={message.from} message={message}/>;
     })}</ul>
 }
