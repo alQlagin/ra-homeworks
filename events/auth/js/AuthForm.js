fc5c72c689edd356a683f4251c873999d88b39d4
Alex Kulagin <al.qlagin@gmail.com>
2018-03-23 15:19:53 +0500

    added AuthForm

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,24 @@
 'use strict';
+const AuthForm = ({onAuth}) => {
+    const userDTO = {};
+    const onSubmit = (event) => {
+        event.preventDefault();
+        onAuth(userDTO);
+    };
+
+    const onChangeFactory = field => event => userDTO[field] = event.target.value;
+
+    return <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onSubmit}>
+        <InputField name={'name'} placehoder={'Имя'} required onChange={onChangeFactory('name')}/>
+        <InputField name={'email'} placehoder={'Электронная почта'} type={'email'} onChange={onChangeFactory('email')}/>
+        <InputField name={'password'} placehoder={'Пароль'} type={'password'} onChange={onChangeFactory('password')}/>
+        <button type="submit"><span>Войти</span><i className="fa fa-fw fa-chevron-right"></i></button>
+    </form>
+};
+
+const InputField = ({onChange, name = null, required = false, type = 'text', placeholder = ''}) => {
+    return <div className="Input">
+        <input required={required} type={type} placeholder={placeholder} onChange={onChange} name={name}/>
+        <label></label>
+    </div>
+};
