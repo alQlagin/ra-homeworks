60815f344f5c82c78e216044ba0d69006bd7b6ae
Alex Kulagin <al.qlagin@gmail.com>
2018-04-01 14:53:05 +0500

    add "layouts" state

--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,44 +4,56 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="toolbar">
-          <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
-        </div>
-        {this.renderLayout(true)}
-      </div>
-    );
-  }
 
-  renderLayout(cardView) {
-    if (cardView) {
-      return (
-        <CardsView
-          layout={this.props.layout}
-          cards={this.getShopItems(this.props.products, cardView)} />
-      );
+    onSwitch = () => {
+        this.setState({cardView: !this.state.cardView});
+    };
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            cardView: true
+        }
     }
-    return (<ListView items={this.getShopItems(this.props.products, cardView)} />);
-  }
 
-  getShopItems(products, cardView) {
-    return products.map(product => {
-      let cardProps = {
-        title: product.name,
-        caption: product.color,
-        img: product.img,
-        price: `$${product.price}`
-      };
-      if (cardView) {
+    render() {
         return (
-          <ShopCard {...cardProps}/>
+            <div>
+                <div className="toolbar">
+                    <IconSwitch
+                        icon={this.state.cardView ? VIEW_MODULE : VIEW_LIST}
+                        onSwitch={this.onSwitch}/>
+                </div>
+                {this.renderLayout(this.state.cardView)}
+            </div>
         );
-      }
-      return (<ShopItem {...cardProps}/>)
-    });
-  }
+    }
+
+    renderLayout(cardView) {
+        if (cardView) {
+            return (
+                <CardsView
+                    layout={this.props.layout}
+                    cards={this.getShopItems(this.props.products, cardView)}/>
+            );
+        }
+        return (<ListView items={this.getShopItems(this.props.products, cardView)}/>);
+    }
+
+    getShopItems(products, cardView) {
+        return products.map(product => {
+            let cardProps = {
+                title: product.name,
+                caption: product.color,
+                img: product.img,
+                price: `$${product.price}`
+            };
+            if (cardView) {
+                return (
+                    <ShopCard {...cardProps}/>
+                );
+            }
+            return (<ShopItem {...cardProps}/>)
+        });
+    }
 }
