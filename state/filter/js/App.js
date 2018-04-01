56f689f6f73289f6d516d59b06bd23c1ba937fac
Alex Kulagin <al.qlagin@gmail.com>
2018-04-01 14:39:20 +0500

    add "filters" with state

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,35 @@
-'use strict'
+'use strict';
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            filter: props.filters[0],
+            projects: this.filterProjects(props.filters[0])
+        };
+
+    }
+
+    onSelectFilter = (filter) => {
+        this.setState({filter, projects: this.filterProjects(filter)})
+    };
+
+    filterProjects(filter) {
+        if (!filter || /all/i.test(filter)) {
+            return [...this.props.projects]
+        } else {
+            const filterRe = new RegExp(`^${filter}$`, 'i');
+            return this.props.projects.filter(project => filterRe.test(project.category))
+        }
+    }
+
+    render() {
+        return <div>
+            <Toolbar
+                filters={this.props.filters}
+                selected={this.state.filter}
+                onSelectFilter={this.onSelectFilter}/>
+            <Portfolio projects={this.state.projects}/>
+        </div>
+    }
+}
